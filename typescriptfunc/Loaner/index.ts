22805bffacc66e4c2763db9e5a554e969e99f8e6
vkstrm <mongomoves@gmail.com>
2021-10-02 15:45:46 +0200

    wip

--- typescriptfunc/Loaner/index.ts
+++ typescriptfunc/Loaner/index.ts
@@ -1,31 +1,56 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as storage from 'azure-storage';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const name = (req.query.name || (req.body && req.body.name));
-    let book: TableBookEntry = context.bindings.tableBook;
-
+const httpTrigger: AzureFunction = async function (context: Context, tableBook: TableBookEntry): Promise<void> {
+    if (tableBook == undefined) {
+        context.res = {
+            status: 404,
+            body: "No such book available, sorry!"
+        }
+        return
+    }
+    
+    if (tableBook.Available <= 0) {
+        context.res = {
+            status: 404,
+            body: "The book is all loaned out!"
+        }
+        return
+    }
+    
+    tableBook.Available = tableBook.Available - 1;
     context.res = {
         status: 200,
-        body: JSON.stringify(new BookResponse())
+        body: JSON.stringify(tableBook.ToBookResponse())
     };
 };
 
 class TableBookEntry {
-    PartitionKey: string
-    RowKey: string
-    Title: string
-    AuthorLastName: string
-    AuthorFirstName: string
-    Available: string
+    PartitionKey: string;
+    RowKey: string;
+    Title: string;
+    AuthorLastName: string;
+    AuthorFirstName: string;
+    Available: number;
+
+    ToBookResponse(): BookResponse {
+        let book = new BookResponse();
+        book.isbn = this.RowKey;
+        book.title = this.Title;
+        book.author_firstname = this.AuthorFirstName;
+        book.author_lastname = this.AuthorLastName;
+        book.available = this.Available
+        return book;
+    }
 }
 
 class BookResponse {
-    isbn: string
-    title: string
-    author_lastname: string
-    author_firstname: string
-    available: string
-    cover_url: string
+    isbn: string;
+    title: string;
+    author_lastname: string;
+    author_firstname: string;
+    available: number;
+    cover_url: string;
 }
 
 export default httpTrigger;
